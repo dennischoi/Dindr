d776a41960948afa9981df618e6ec8de8fcae723
Kimberly Oleiro <koleiro@gmail.com>
2016-05-10 15:18:03 -0400

    Updated Seeds with Tags and Categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,12 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+User.destroy_all
+Cuisine.destroy_all
+Tag.destroy_all
+Picture.destroy_all
 
+# Users
 User.create({
   first_name: 'Mark',
   last_name: 'Wong',
@@ -18,22 +23,88 @@ User.create({
   phone_number: '416-555-6666'
   })
 
-
-Tag.create({
-  name: 'Spicy'
-  })
+# Cuisines (10)
 
 Cuisine.create({
   name: 'Mexican'
   })
 
-Picture.create({
-  url: 'www.facebook.com',
-  cuisine_id: Cuisine.find_by(name: 'Mexican').id,
+Cuisine.create({
+  name: 'Thai'
   })
+
+Cuisine.create({
+  name: 'Japanese'
+  })
+
+Cuisine.create({
+  name: 'Italian'
+  })
+
+Cuisine.create({
+  name: 'Chinese'
+  })
+
+Cuisine.create({
+  name: 'American'
+  })
+
+Cuisine.create({
+  name: 'Vegetarian'
+  })
+
+Cuisine.create({
+  name: 'Indian'
+  })
+
+Cuisine.create({
+  name: 'Sandwiches'
+  })
+
+Cuisine.create({
+  name: 'Mediterranean'
+  })
+
+
+# Tags (8)
+
+spicy = Tag.create({
+  name: 'Spicy'
+  })
+
+comfort = Tag.create({
+  name: 'Comfort'
+  })
+
+dessert = Tag.create({
+  name: 'Dessert'
+  })
+
+healthy = Tag.create({
+  name: 'Healthy'
+  })
+
+vegan = Tag.create({
+  name: 'Vegan'
+  })
+
+sweet = Tag.create({
+  name: 'Sweet'
+  })
+
+savoury = Tag.create({
+  name: 'Savoury'
+  })
+
+fried = Tag.create({
+  name: 'Fried'
+  })
+
+
+#Pictures
+
 
 PicTag.create({
   picture_id: Picture.find_by(url: 'www.facebook.com').id,
   tag_id: Tag.find_by(name: 'Spicy').id
-
   })
