9036f30c8ff7693c9645d6cbfd9ced106e184717
Dennis Choi <dennis.choi7@gmail.com>
2016-05-11 00:51:49 -0400

    - Added to the JS functions of the on click of hearts and X’s 		- input value changes between true and false depending on which is press, and changes colour accordingly
    - Basic css changes to line up the like and dislike on click spaces
    
    - Changed images to tags for potential overlay options
    	- added id to input tags to be able to select them on js

--- app/assets/javascripts/homes.js
+++ app/assets/javascripts/homes.js
@@ -4,6 +4,9 @@ $(document).on('ready page:load', function() {
   function likeOne(ev) {
     console.log("Clicked")
       $('#food-1').addClass('like');
+      $(this).css("background-color","green");
+      $('#dislike-button1').css("background-color", "transparent");
+      $('#rand-1').attr("value", "true");
       $('#food-1').removeClass('dislike');
 
       $('#food-1').off('click');
@@ -15,6 +18,9 @@ $('#like-button1').on('click', likeOne)
     console.log('Click 2')
 
     $('#food-2').addClass('like');
+    $(this).css("background-color","green");
+    $('#dislike-button2').css("background-color", "transparent");
+    $('#rand-2').attr("value", "true");
     $('#food-2').removeClass('dislike');
 
     $('#food-2').off('click');
@@ -27,6 +33,9 @@ $('#like-button2').on('click', likeTwo)
     console.log('Click 3')
 
     $('#food-3').addClass('like');
+    $(this).css("background-color","green");
+    $('#dislike-button3').css("background-color", "transparent");
+    $('#rand-3').attr("value", "true");
     $('#food-3').removeClass('dislike');
 
     $('#food-3').off('click');
@@ -39,6 +48,9 @@ $('#like-button3').on('click', likeThree)
     console.log('Dislike 1')
 
       $('#food-1').addClass('dislike');
+      $(this).css("background-color","red");
+      $('#like-button1').css("background-color", "transparent");
+      $('#rand-1').attr("value", "false");
       $('#food-1').removeClass('like');
 
       $('#food-1').off('click');
@@ -51,6 +63,9 @@ $('#dislike-button1').on('click', dislikeOne)
     console.log('Dislike 2')
 
       $('#food-2').addClass('dislike');
+      $(this).css("background-color","red");
+      $('#like-button2').css("background-color", "transparent");
+      $('#rand-2').attr("value", "false");
       $('#food-2').removeClass('like');
 
       $('#food-2').off('click');
@@ -63,6 +78,9 @@ $('#dislike-button2').on('click', dislikeTwo)
     console.log('Dislike 3')
 
       $('#food-3').addClass('dislike');
+      $(this).css("background-color","red");
+      $('#like-button3').css("background-color", "transparent");
+      $('#rand-3').attr("value", "false");
       $('#food-3').removeClass('like');
 
       $('#food-3').off('click');
