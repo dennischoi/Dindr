b434add866ec393d802d817d0f9ef4965fabd291
Kimberly Oleiro <koleiro@gmail.com>
2016-05-11 17:50:07 -0400

    merged branch that included javascript for liked pictures

--- app/assets/javascripts/pictures.js
+++ app/assets/javascripts/pictures.js
@@ -21,4 +21,54 @@ $(document).on('ready', function() {
       console.log(turn);
     };
   });
+
+  $('#food-1').on('click', likeOne)
+
+  function likeOne(ev) {
+    console.log("Clicked")
+      if ($(this).hasClass('dislike')){
+        $('#rand-1').attr("value", "true");
+        $(this).attr('class','like');
+        $(this).css('border', '5px solid green');
+      }
+      else if ($(this).hasClass('like')){
+        $('#rand-1').attr("value", "false");
+        $(this).attr('class','dislike');
+        $(this).css('border', 'transparent');
+      }
+  };
+
+
+  $('#food-2').on('click', likeTwo)
+
+  function  likeTwo(ev) {
+    console.log("Clicked")
+      if ($(this).hasClass('dislike')){
+        $('#rand-2').attr("value", "true");
+        $(this).attr('class','like');
+        $(this).css('border', '5px solid green');
+      }
+      else if ($(this).hasClass('like')){
+        $('#rand-2').attr("value", "false");
+        $(this).attr('class','dislike');
+        $(this).css('border', 'transparent');
+      }
+  };
+
+  $('#food-3').on('click', likeThree)
+
+  function likeThree(ev) {
+    console.log("Clicked")
+      if ($(this).hasClass('dislike')){
+        $('#rand-3').attr("value", "true");
+        $(this).attr('class','like');
+        $(this).css('border', '5px solid green');
+      }
+      else if ($(this).hasClass('like')){
+        $('#rand-3').attr("value", "false");
+        $(this).attr('class','dislike');
+        $(this).css('border', 'transparent');
+      }
+  };
+
 });
