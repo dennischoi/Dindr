f492a145d14872aa4f71c6042a6faf66a2a183fe
Kimberly Oleiro <koleiro@gmail.com>
2016-05-11 17:38:53 -0400

    Created next button and started ajax request

--- app/assets/javascripts/homes.js
+++ app/assets/javascripts/homes.js
@@ -8,10 +8,12 @@ $(document).on('ready page:load', function() {
       if ($(this).hasClass('dislike')){
         $('#rand-1').attr("value", "true");
         $(this).attr('class','like');
+        $(this).css('border', '5px solid green');
       }
       else if ($(this).hasClass('like')){
         $('#rand-1').attr("value", "false");
         $(this).attr('class','dislike');
+        $(this).css('border', 'transparent');
       }
   };
 
@@ -21,10 +23,12 @@ $(document).on('ready page:load', function() {
       if ($(this).hasClass('dislike')){
         $('#rand-2').attr("value", "true");
         $(this).attr('class','like');
+        $(this).css('border', '5px solid green');
       }
       else if ($(this).hasClass('like')){
         $('#rand-2').attr("value", "false");
         $(this).attr('class','dislike');
+        $(this).css('border', 'transparent');
       }
   };
 
@@ -35,13 +39,33 @@ $(document).on('ready page:load', function() {
       if ($(this).hasClass('dislike')){
         $('#rand-3').attr("value", "true");
         $(this).attr('class','like');
+        $(this).css('border', '5px solid green');
       }
       else if ($(this).hasClass('like')){
         $('#rand-3').attr("value", "false");
         $(this).attr('class','dislike');
+        $(this).css('border', 'transparent');
       }
   };
 
   $('#food-3').on('click', likeThree)
+
+
+
+  $('#next-button').on('click', nextButton)
+
+  function nextButton(ev) {
+    alert("hiii you're doing great!")
+    ev.preventDefault();
+
+    $.ajax({
+      type: "get",
+      url: "/homes",
+      dataType: "html",
+      success: function(){
+        alert("you're the best!")
+      }
+    })
+  }
 
 });
