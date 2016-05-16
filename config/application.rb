828dfd11e6568763a7c4951caa338f57a03842bf
Dennis Choi <dennis.choi7@gmail.com>
2016-05-16 19:15:45 -0400

    - Implemented a mailer to be sent out to new users that sign up (gets a welcome email)

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module Dindr
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-
+    # config.action_mailer.default_url_options = { host: 'http://localhost:3000/'}
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
