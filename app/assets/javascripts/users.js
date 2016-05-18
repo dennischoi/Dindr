ea552f8ef2610f340e0138f56a26c14a15e9d609
Kimberly Oleiro <koleiro@gmail.com>
2016-05-18 17:14:54 -0400

    We created an ajax request to the yelp api to return cuisine from category filter

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,12 +1,35 @@
-$(document).on('ready', function(){
+$(document).on('ready load: page', function(){
+  $("#nearby-restaurants").on('click', function(){
 
-  if ("geolocation" in navigator) {
-    /* geolocation is available */
-  } else {
-    /* geolocation IS NOT available */
-  }
+    if ('geolocation' in navigator){
+      navigator.geolocation.getCurrentPosition(success, failure);
+    } else {
+      alert ("Geolocation not supported");
+    }
+  });
+});
 
 
+function success(position){
+  console.log(position.coords.latitude);
+  console.log(position.coords.longitude);
+
+  var cuisine = $('#nearby-restaurants').data('cuisinename')
+
+  $.ajax({
+    url: '/restaurants',
+    method: 'get',
+    dataType: 'html',
+    data: {
+        category_filter: cuisine
+      },
+    success: function(data){
+      console.log(data);
+    }
+  });
+}
 
 
-})
+function failure(err){
+  console.log('ERROR(' + err.code + '): ' + err.message);
+}
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,19 +3,32 @@ class RestaurantsController < ApplicationController
   def index
 
 
+    #
+    # if request.xhr?
+    #   params[:category_filter]
+
     search_options = {
            limit: 2,
-           category_filter: 'mexican',
+           category_filter: params[:category_filter].downcase
          }
+
+    # else
+    #   flash.now[:alert] = "The fuck"
+    # end
 
     bounding_box = { sw_latitude: 43.647303, sw_longitude: -79.393374, ne_latitude: 43.656440, ne_longitude: -79.376840 }
 
 
-    restaurants = Yelp.client.search_by_bounding_box(bounding_box, search_options).businesses
+    restaurants =
+    Yelp.client.search_by_bounding_box(bounding_box, search_options).businesses
 
     @restaurant_list = []
     restaurants.each {|restaurant| @restaurant_list << [restaurant.name, restaurant.url]}
-  end
+
+    respond_to do |format|
+      format.html
+    end
+    end
 
   def show
   end
