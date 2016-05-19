53b06c62cd2052206c19196a71e92915f73cb373
Ariane Deschenes Foucher <ariane.df@me.com>
2016-05-19 18:12:00 -0400

    Still troubleshooting UserMailer in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'gmail.com',
+    domain:               'arcane-ocean-79595.herokuapp.com',
     user_name:            'dindr.teamveg@gmail.com',
     password:             'sideroom',
     authentication:       'plain',
