56f5bb88f4e64e7e980f1dd72c817fe0cd72c358
houtaiw61@gmail.com <houtaiw61@gmail.com>
2016-05-25 11:36:45 -0400

    changed the tenses of the association methods to make it cleaner

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,4 +1,4 @@
 class Invitation < ActiveRecord::Base
-  belongs_to :send_user, class_name: 'User'
-  belongs_to :accept_user, class_name: 'User'
+  belongs_to :sent_user, class_name: 'User'
+  belongs_to :accepted_user, class_name: 'User'
 end
