237fe36ffa0c8ce79ee63f3d12d9639082617138
michaelkthai <mthai27@gmail.com>
2016-05-28 16:01:13 -0400

    added working links to new navbar - all working except for sign in modal

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -62,29 +62,3 @@ $(window).resize(function() {
     }
   }
 });
-
-
-// ----------------------------------------------------------------------------
-// BURGER NAV
-// ----------------------------------------------------------------------------
-
-  ( function () {
-      $('.menu-trigger').mouseenter(function() {
-          $(this).toggleClass('active');
-          // $('#nav-box').toggle();
-          $('#nav-box').css('visibility', 'visible');
-      });
-  }.call(this));
-
-  ( function () {
-    $('.navigation').mouseleave(function() {
-      $('.menu-trigger').toggleClass('active');
-      // $('#nav-box').toggle();
-      $('#nav-box').css('visibility', 'hidden');
-      // $('#nav-box').css('color', 'green');
-    });
-  }.call(this));
-
-
-
-});
---
+++ app/assets/javascripts/homes.js
@@ -0,0 +1,22 @@
+// ----------------------------------------------------------------------------
+// BURGER NAV
+// ----------------------------------------------------------------------------
+$(document).ready(function() {
+  ( function () {
+        $('.menu-trigger').mouseenter(function() {
+            $(this).toggleClass('active');
+            // $('#nav-box').toggle();
+            $('#nav-box').css('visibility', 'visible');
+        });
+    }.call(this));
+
+    ( function () {
+      $('.navigation').mouseleave(function() {
+        $('.menu-trigger').toggleClass('active');
+        // $('#nav-box').toggle();
+        $('#nav-box').css('visibility', 'hidden');
+        // $('#nav-box').css('color', 'green');
+      });
+    }.call(this));
+
+  });
