827d246696f62042c7d9e38f87812828947c9f40
michaelkthai <mthai27@gmail.com>
2016-05-30 03:35:45 -0400

    removed bourbon’s button stylings. made a new animated button for the homepage and have sign-in modal working.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,63 +2,47 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+//= require "burger"
 
 $(document).ready(function() {
 // ----------------------------------------------------------------------------
 // MODAL
 // ----------------------------------------------------------------------------
-$(function() {
-  $("#modal-1").on("change", function() {
-    if ($(this).is(":checked")) {
-      $("body").addClass("modal-open");
-    } else {
-      $("body").removeClass("modal-open");
-    }
-  });
 
-  $(".modal-fade-screen, .modal-close").on("click", function() {
-    $(".modal-state:checked").prop("checked", false).change();
-  });
 
-  $(".modal-inner").on("click", function(e) {
-    e.stopPropagation();
-  });
-});
+  $(function() {
+    $("#button").on("click", function() {
+        $(".modal").css("display", "inherit");
+    });
 
-$(function() {
-  $("#open-sign-in").on("click", function(e) {
-      e.preventDefault();
-      $("body").addClass("modal-open");
-  });
+    $(".modal-fade-screen, .modal-close").on('click', function() {
+      $(".modal").css("display", "none");
+    });
 
-  $(".modal-fade-screen, .modal-close").on('click', function() {
-    $("body").removeClass("modal-open");
-  });
-
-  $('.modal-inner').on('click', function(e) {
-    e.stopPropagation();
+    $('.modal-inner').on('click', function(e) {
+      e.stopPropagation();
+    });
   });
 });
-
 // ----------------------------------------------------------------------------
 // NAV
 // ----------------------------------------------------------------------------
 
-$(window).resize(function() {
-  var more = document.getElementById("js-navigation-more");
-  if ($(more).length > 0) {
-    var windowWidth = $(window).width();
-    var moreLeftSideToPageLeftSide = $(more).offset().left;
-    var moreLeftSideToPageRightSide = windowWidth - moreLeftSideToPageLeftSide;
-
-    if (moreLeftSideToPageRightSide < 330) {
-      $("#js-navigation-more .submenu .submenu").removeClass("fly-out-right");
-      $("#js-navigation-more .submenu .submenu").addClass("fly-out-left");
-    }
-
-    if (moreLeftSideToPageRightSide > 330) {
-      $("#js-navigation-more .submenu .submenu").removeClass("fly-out-left");
-      $("#js-navigation-more .submenu .submenu").addClass("fly-out-right");
-    }
-  }
-});
+// $(window).resize(function() {
+//   var more = document.getElementById("js-navigation-more");
+//   if ($(more).length > 0) {
+//     var windowWidth = $(window).width();
+//     var moreLeftSideToPageLeftSide = $(more).offset().left;
+//     var moreLeftSideToPageRightSide = windowWidth - moreLeftSideToPageLeftSide;
+//
+//     if (moreLeftSideToPageRightSide < 330) {
+//       $("#js-navigation-more .submenu .submenu").removeClass("fly-out-right");
+//       $("#js-navigation-more .submenu .submenu").addClass("fly-out-left");
+//     }
+//
+//     if (moreLeftSideToPageRightSide > 330) {
+//       $("#js-navigation-more .submenu .submenu").removeClass("fly-out-left");
+//       $("#js-navigation-more .submenu .submenu").addClass("fly-out-right");
+//     }
+//   }
+// });
