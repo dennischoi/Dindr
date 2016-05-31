5882553a4cadfb020e52b9213e84797375a7fef3
houtaiw61@gmail.com <houtaiw61@gmail.com>
2016-05-31 18:41:02 -0400

    Made a validation so that a user cant send multiple invitations to the same user

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,4 +1,21 @@
 class Invitation < ActiveRecord::Base
-  belongs_to :sent_user, class_name: 'User'
+  belongs_to :send_user, class_name: 'User'
   belongs_to :accepted_user, class_name: 'User'
+
+  validate :same_date
+
+  def same_date
+    user = self.send_user
+    a = user.sent_invitations
+    b = user.accepted_invitations
+
+    inv_arr = a + b
+    # binding.pry
+
+    inv_arr.each do |inv|
+      if self.send_user_id == inv.send_user_id && self.accept_user_id == inv.accept_user_id
+        errors.add(:sent_user, "you've already matched!")
+      end
+    end
+  end
 end
