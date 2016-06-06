5f1b36a2249161f365bc09618d8c3029405c8ecd
Kimberly Oleiro <koleiro@gmail.com>
2016-06-06 15:21:00 -0400

    Fixed bug in users controller to skip over Mark down-to-meet default true"

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,9 @@ class UsersController < ApplicationController
 
 
     array_users.each do |user|
+      if user.top_cats.empty?
+        next
+      else
         cuisine_A = user.top_cats
         if cuisine_A[0].include?(@cuis)
           potentials << user.id
@@ -19,6 +22,7 @@ class UsersController < ApplicationController
           potentials << user.id
         else
         end
+      end
     end
 
     inv_id = []
